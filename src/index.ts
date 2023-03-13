f8dde1418daa447684ab392ec971e2cc3cd00c41
Omar Mohammad <omarmo@cloudflare.com>
2023-03-13 14:28:05 +0000

    Replace fetch with manager.fetch

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { ComponentSettings, Manager } from '@managed-components/types'
 import { getEcommerceRequestBody } from './ecommerce'
 import { getRequestBody } from './track'
 
-const sendEvent = async (payload: any, settings: ComponentSettings) => {
+const sendEvent = async (
+  payload: any,
+  manager: Manager,
+  settings: ComponentSettings
+) => {
   const property = payload.custom_data.property || settings.property
   const graphEndpoint = `https://graph.facebook.com/v14.0/${property}/events`
 
@@ -14,7 +18,7 @@ const sendEvent = async (payload: any, settings: ComponentSettings) => {
     }),
   }
 
-  fetch(graphEndpoint, {
+  manager.fetch(graphEndpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -26,16 +30,16 @@ const sendEvent = async (payload: any, settings: ComponentSettings) => {
 export default async function (manager: Manager, settings: ComponentSettings) {
   manager.addEventListener('event', async event => {
     const request = await getRequestBody('event', event, settings)
-    sendEvent(request, settings)
+    sendEvent(request, manager, settings)
   })
 
   manager.addEventListener('pageview', async event => {
     const request = await getRequestBody('pageview', event, settings)
-    sendEvent(request, settings)
+    sendEvent(request, manager, settings)
   })
 
   manager.addEventListener('ecommerce', async event => {
     const request = await getEcommerceRequestBody(event, settings)
-    sendEvent(request, settings)
+    sendEvent(request, manager, settings)
   })
 }
